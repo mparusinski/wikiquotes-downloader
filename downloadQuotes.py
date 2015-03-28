3cd22abcb7981908916d6eba246975bd8ca6269f
Michal Parusinski <mparusinski@gmail.com>
2015-03-28 14:53:32 +0000

    Added the ability to get the internal representation instead of the final son string

--- downloadQuotes.py
+++ downloadQuotes.py
@@ -1,27 +1,29 @@
 # coding=UTF-8
 import sys
+import argparse
 
 from WikiquotesRetriever import *
 from IRBuilder import *
 from CleanIR import *
 from IRToJson import *
 
-if len(sys.argv) > 1:
-	try:
-		philosophersName = sys.argv[1]
-		wikiRetriever = WikiquotesRetriever()
-		wikiRetriever.setupNetworking()
-		jsonContent = wikiRetriever.downloadQuote(philosophersName)
-		wikiRetriever.closeNetworking()
-		wikitext = Wikitext(jsonContent)
-		irinstance = WikitextIR(wikitext)
-		removeNoiseQuotes(irinstance)
-		removeTranslations(irinstance)
-		cleanIR(irinstance)
-		jsonString = createJSONFromIR(irinstance)
-		print jsonString
-	except:
-		print "Exception occurred"
+parser = argparse.ArgumentParser(description="Download all quotes from a certain philosopher")
+parser.add_argument('author', action="store", metavar='author', type=str, help='Name of the author you wish download quotes from')
+parser.add_argument('--raw', action="store_true", dest="raw", help='Show internal representation obtained from wikiquotes with no parsing', default=False)
+args = parser.parse_args()
+
+philosophersName = args.author
+wikiRetriever = WikiquotesRetriever()
+wikiRetriever.setupNetworking()
+jsonContent = wikiRetriever.downloadQuote(philosophersName)
+wikiRetriever.closeNetworking()
+wikitext = Wikitext(jsonContent)
+irinstance = WikitextIR(wikitext)
+if args.raw:
+	print irinstance.toString()
 else:
-	# TODO Complain about syntax (something better than what is written below)
-	print "Syntax is incorrect"
+	removeNoise(irinstance)
+	removeTranslations(irinstance)
+	cleanIR(irinstance)
+	jsonString = createJSONFromIR(irinstance)
+	print jsonString
