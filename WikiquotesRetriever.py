7dedb81dac0b53ff875c3873c9df3a161c7870d6
Michal Parusinski <mparusinski@gmail.com>
2015-03-28 18:35:26 +0000

    Improved style of WikiquotesRetriever

--- WikiquotesRetriever.py
+++ WikiquotesRetriever.py
@@ -2,10 +2,12 @@ import pycurl
 from StringIO import StringIO
 
 PEEK_AHEAD_FOR_ERROR_LENGTH = 321
+WIKIQUOTE_URL_BASE = "http://en.wikiquote.org/w/api.php?format=json&action=query&titles="
 
 class InvalidTitleException(Exception):
 
     def __init__(self, title):
+        super(InvalidTitleException, self).__init__(title)
         self.title = title
 
     def __str__(self):
@@ -15,9 +17,9 @@ class InvalidTitleException(Exception):
 class WikiquotesRetriever(object):
     """This class retrieves pages from Wikiquotes"""
     def __init__(self):
-        self.title_place_holder   = "%%TITLE%%"
-        self.wikiquote_url_format = "http://en.wikiquote.org/w/api.php?format=json&action=query&titles=" \
-            + self.title_place_holder + "&prop=revisions&rvprop=content"
+        self.title_place_holder = "%%TITLE%%"
+        self.wikiquote_url = WIKIQUOTE_URL_BASE + self.title_place_holder \
+            + "&prop=revisions&rvprop=content"
         self.curler = None
 
     def setup_networking(self):
@@ -26,32 +28,23 @@ class WikiquotesRetriever(object):
     def close_networking(self):
         self.curler.close()
 
-    def __preprocess_title(self, title):
-        """ For instance "Friedrich Nietzsche" would become "Friedrich_Nietzsche" """
-        return title.replace(' ','%20')
-
-    def __getURL(self, title):
-        url_ready_title = self.__preprocess_title(title)
-        url_for_title = self.wikiquote_url_format.replace(self.title_place_holder, url_ready_title)
+    def __get_url(self, title):
+        url_ready_title = title.replace(' ', '%20')
+        url_for_title = self.wikiquote_url.replace(self.title_place_holder, url_ready_title)
         return url_for_title
 
     def download_quote(self, title):
-        buffer = StringIO()
-        self.curler.setopt(self.curler.URL, self.__getURL(title))
-        self.curler.setopt(self.curler.WRITEDATA, buffer)
+        buffer_string = StringIO()
+        self.curler.setopt(self.curler.URL, self.__get_url(title))
+        self.curler.setopt(self.curler.WRITEDATA, buffer_string)
         self.curler.perform()
-        json_text = buffer.getvalue()
+        json_text = buffer_string.getvalue()
         peek_ahead = json_text[0:PEEK_AHEAD_FOR_ERROR_LENGTH]
         peek_text = "\"pages\":{\"-1\""
         if peek_text in peek_ahead:
             raise InvalidTitleException(title)
         return json_text
 
-def main():
-    wiki_retriever = WikiquotesRetriever()
-    wiki_retriever.setup_networking()
-    print wiki_retriever.download_quote("Friedrich Nietzsche")
-    wiki_retriever.close_networking()
 
 if __name__ == '__main__':
-    main()
+    pass
