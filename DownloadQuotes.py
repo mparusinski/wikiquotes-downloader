fe4e866b564a48f9ddc7535214ee7ff59a6a552b
Michal Parusinski <mparusinski@gmail.com>
2015-04-04 16:14:48 +0200

    Added option in main (DownloadQuotes)
    The new option allows to specify to use a JSON file instead of connecting to
    wikiquote website. This is useful for testing as it does not require an
    internet connection only a pre-downloaded JSON file that the wikiquote website
    provides through its API.

--- DownloadQuotes.py
+++ DownloadQuotes.py
@@ -16,24 +16,38 @@ def main():
     parser.add_argument('--raw', action="store_true", dest="raw", \
         help='Show internal representation obtained from wikiquotes with no parsing', \
         default=False)
+    parser.add_argument('--from-json', action="store", metavar='json_file', type=str, \
+        help='Use JSON file instead of wikiquote API')
     args = parser.parse_args()
     philosophers_name = args.author
     wiki_retriever = WikiquotesRetriever()
     wiki_retriever.setup_networking()
-    try:
-        json_content = wiki_retriever.download_quote(philosophers_name)
-        wiki_retriever.close_networking()
-        irinstance = ir_from_json(json_content)
-        if args.raw:
-            print irinstance
-        else:
-            remove_noise(irinstance)
-            remove_translations(irinstance)
-            clean_ir(irinstance)
-            json_string = create_json_from_ir(irinstance)
-            print json_string.encode('UTF-8')
-    except NetworkingException as e:
-        print e
+    json_content = ""
+    if args.from_json:
+        try:
+            json_file_path = args.from_json
+            fhandle = open(json_file_path, "r")
+            json_content = fhandle.read()
+        except Exception as e:
+            print e
+            return
+    else:
+        try:
+            json_content = wiki_retriever.download_quote(philosophers_name)
+            wiki_retriever.close_networking()
+        except NetworkingException as e:
+            print e
+            return
+    irinstance = ir_from_json(json_content)
+    if args.raw:
+        print irinstance
+    else:
+        remove_noise(irinstance)
+        remove_translations(irinstance)
+        clean_ir(irinstance)
+        json_string = create_json_from_ir(irinstance)
+        print json_string.encode('UTF-8')
+
 
 if __name__ == '__main__':
     main()
